d239f51506bb25a0f3fc789e7ed3fb7db2ef60c0
Miguel <azurduy.campos.josemiguel@gmail.com>
2022-10-30 20:00:02 -0400

    CRUD Servicios

--- database/migrations/2022_10_30_151928_create_servicios_table.php
+++ database/migrations/2022_10_30_151928_create_servicios_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('servicios', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('descripcion')->nullable();
+            $table->double('precio',9,2)->nullable();
             $table->timestamps();
         });
     }
